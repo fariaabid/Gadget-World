189bb1f2a977b0ccd02aa6da46235c5282901516
faria abid <faria.ju.2k16@gmail.com>
2021-04-08 11:24:14 +0600

    half done

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="text-center my-5 py-5">
+            <p> &copy; All rights reserved by Faria Abid, {new Date().getFullYear()}</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Components/SignIn/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAUnDNJVKPh2VVn5oecgxV2dyV2Huxdtqk",
+    authDomain: "gadget-world-8d027.firebaseapp.com",
+    projectId: "gadget-world-8d027",
+    storageBucket: "gadget-world-8d027.appspot.com",
+    messagingSenderId: "313379947836",
+    appId: "1:313379947836:web:8d32bd3c2bbece5ced8a39"
+  };
+  export default firebaseConfig;
